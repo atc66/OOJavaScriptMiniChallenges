34ac80704e0ef54e9e81ef98e26ca2fca0fbaf68
aciukurescu <andrewciukurescu@gmail.com>
2018-01-23 17:52:01 -0500

    exercise 2 complete

--- index.js
+++ index.js
@@ -26,28 +26,25 @@
 // Each Photo should store the photo's file name and the location the photo was taken in as strings. 
 // Create instances of each object defined to prove that your object model works.
 
-// function Album (){
-// 	this.photos = []
-// 	this.addPhoto = function(photo){
-// 		let photo = this.photos.length;
-// 		this.photos[i] = photo;
-// 	}
+function Album (name) {
+	this.name = name
+	this.pics = []
+	this.addPhoto = function(x){
+		this.pics.push(x)
+		console.log('A new photo has been added to your album')
+	}
+}
 
-// 	this.listAllPhotos = function(){
-// 		for(i=photo-1; i>=0; i--) {
-// 			console.log(this.photos[i].name);
-// 		};
-// 	}
+class Photos {
+	constructor (name, location){
+		this.name = name;
+		this.location = location;
+	}
+}
 
-// 	this.specificPhoto = function(photo_location){
-// 		console.log(this.photos[photo_location - 1]);
-// 	}
-// }
-
-// function Photo (name, location) {
-// 	this.name = name;
-// 	this.location = location;
-// }
+var eiffelTower = new Photos('EiffelTower', 'Paris, France')
+var grandCanyon = new Photos('Grand Canyon', 'AZ, USA')
+var myAlbum = new Album('Andrews Album')
 
 // Exercise 3
 // Create a prototypical Person object. From this object, extend a Teacher object and a Student object. 
@@ -56,51 +53,51 @@
 // Make sure to write code afterwards that creates instances of these objects to make sure that what 
 // you've written works well and you're able to store the necessary data in each object.
 
-var d = new Date ();
-var n = d.getFullYear();
+// var d = new Date ();
+// var n = d.getFullYear();
 
-class Person {
-	constructor (name, age, location){
-		this.name = name;
-		this.age = age;
-		this.location = location;
-	}
-}
+// class Person {
+// 	constructor (name, age, location){
+// 		this.name = name;
+// 		this.age = age;
+// 		this.location = location;
+// 	}
+// }
 
-class Student extends Person {
-	constructor(name, age, location, grade){
-		super(name, age, location)
-		this.grade = grade
-		} yearBorn(){
-			return n-(this.age)
-	}
-}
+// class Student extends Person {
+// 	constructor(name, age, location, grade){
+// 		super(name, age, location)
+// 		this.grade = grade
+// 		} yearBorn(){
+// 			return n-(this.age)
+// 	}
+// }
 
-class Teacher extends Person {
-	constructor(name, age, location, subject){
-		super(name, age, location)
-		this.subjet = subject
-		} yearBorn(){
-			return n-(this.age);
-	}
-}
+// class Teacher extends Person {
+// 	constructor(name, age, location, subject){
+// 		super(name, age, location)
+// 		this.subjet = subject
+// 		} yearBorn(){
+// 			return n-(this.age);
+// 	}
+// }
 
-function School (name){
-	this.name = name
-	this.teachers = []
-	this.students = []
-	this.addTeacher = function(x){
-		this.teachers.push(x)
-		console.log('A new teacher was hired at Rock and Roll High School')
-	}
-	this.addStudent = function(x){
-		this.students.push(x)
-		console.log('A new student enrolled in Rock and Roll High School')
-	}
-}
+// function School (name){
+// 	this.name = name
+// 	this.teachers = []
+// 	this.students = []
+// 	this.addTeacher = function(x){
+// 		this.teachers.push(x)
+// 		console.log('A new teacher was hired at Rock and Roll High School')
+// 	}
+// 	this.addStudent = function(x){
+// 		this.students.push(x)
+// 		console.log('A new student enrolled in Rock and Roll High School')
+// 	}
+// }
 
-var student1 = new Student('Peter Parker', 15, 'Queens, NY', 'Sophmore')
-var student2 = new Student('Andrew Ciukurescu', 29, 'Philadelphia, PA', 'Post Grad')
-var teacher1 = new Teacher('George Feeny', 60, 'Philadelphia, PA', 'History')
-var teacher2 = new Teacher('Dimitri Islam', 23, 'Buffalo, NY', 'History')
-var school1 = new School('Rock and Roll High School')
+// var student1 = new Student('Peter Parker', 15, 'Queens, NY', 'Sophmore')
+// var student2 = new Student('Andrew Ciukurescu', 29, 'Philadelphia, PA', 'Post Grad')
+// var teacher1 = new Teacher('George Feeny', 60, 'Philadelphia, PA', 'History')
+// var teacher2 = new Teacher('Dimitri Islam', 23, 'Buffalo, NY', 'History')
+// var school1 = new School('Rock and Roll High School')
